04408b40fb8ba614c516b196a449d4b27097af7a
jdomowicz <jdomowicz@salesforce.com>
2023-02-28 20:44:28 +0100

    cocktail tile from api layout

---
+++ .history/force-app/main/default/lwc/cocktailTileFromApi/cocktailTileFromApi_20230228191120.js
@@ -0,0 +1,13 @@
+import { LightningElement,api } from 'lwc';
+
+export default class CocktailTileFromApi extends LightningElement {
+
+    @api cocktail;
+    cocktail
+
+
+    @api cocktailIng(){
+        return 
+    }
+
+}
---
+++ .history/force-app/main/default/lwc/cocktailTileFromApi/cocktailTileFromApi_20230228191639.js
@@ -0,0 +1,16 @@
+import { LightningElement,api } from 'lwc';
+
+export default class CocktailTileFromApi extends LightningElement {
+
+    @api cocktail;
+    Ing1;
+
+
+    get cocktailIng(){
+        return `— ${this.cocktail.Ingredient1__c}`;
+    }
+    get cocktailIng(){
+        return `— ${this.cocktail.Ingredient2__c}`;
+    }
+
+}
